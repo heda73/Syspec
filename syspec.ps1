71fc0d3f729b15481c67e4254c8c8741aea41ba1
HEDA <121900965+heda73@users.noreply.github.com>
2026-07-18 16:27:46 +0200

    Add files via upload

---
+++ syspec.ps1
@@ -0,0 +1,113 @@
+<#
+
+       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣶⣦⣤⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣤⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣿⢿⡿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⠟⠋⠉⠉⠉⠉⠁⠀⠀⠘⠿⠿⣿⣿⣿⢿⣿⣿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⡟⠀⠉⠻⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣠⣶⣿⣿⡿⠿⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡄⠀⠀⠈⢿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠰⠛⠻⠟⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠸⣿⡔⠛⢶⠾⢿⣷⣤⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠿⠿⣷⣶⣶⣿⡿⠇⠈⠉⠉⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣈⢀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+BY JULES KERENTERFF
+
+#>
+
+
+function memory_stat {
+    $memory_installed = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
+    $memory_max = (Get-CimInstance -ClassName Win32_PhysicalMemoryArray).MaxCapacity / 1MB
+    Write-Host "//////////////// Memoire /////////////////" -ForegroundColor Yellow
+    Write-Host "    Memoire installe : $memory_installed " 
+    Write-Host "    Memoire max : $memory_max " 
+
+    $os = Get-CimInstance -ClassName Win32_OperatingSystem
+    $totalRAM = $os.TotalVisibleMemorySize   # en KB
+    $freeRAM = $os.FreePhysicalMemory         # en KB
+    $usedRAM = $totalRAM - $freeRAM
+    $percentUsed = [math]::Round(($usedRAM / $totalRAM) * 100, 2)
+    Write-Host "    Memory Usage : $percentUsed %" -ForegroundColor Green
+}
+
+function prossesore_stat {
+    $pros_name = Get-CimInstance -ClassName Win32_Processor | Select-Object -ExpandProperty Name
+    $pros_speed = Get-CimInstance -ClassName Win32_Processor | Select-Object -ExpandProperty MaxClockSpeed
+    $pros_cores = Get-CimInstance -ClassName Win32_Processor | Select-Object -ExpandProperty NumberOfCores
+    $pros_logic_cores = Get-CimInstance -ClassName Win32_Processor | Select-Object -ExpandProperty NumberOfLogicalProcessors
+    $pros_usage = (Get-CimInstance Win32_PerfFormattedData_PerfOS_Processor | Where-Object Name -eq '_Total').PercentProcessorTime
+    Write-Host "/////////////// Prossesor ///////////////" -ForegroundColor Yellow
+    Write-Host "    Prossesor Name : $pros_name" -ForegroundColor Cyan
+    Write-Host "    Prossesor speed : $([math]::Round($pros_speed/1000,1)) GHz" 
+    Write-Host "    Prossesor cores : $pros_cores" 
+    Write-Host "    Prossesor logic cores : $pros_logic_cores" 
+    Write-Host "    Prossesor Usage : $pros_usage %" -ForegroundColor green
+}
+
+function disk_stat {
+    $disks = Get-CimInstance -ClassName Win32_DiskDrive
+    Write-Host "//////////////////// Disks //////////////////////" -ForegroundColor Yellow
+    foreach ($disk in $disks) {
+        Write-Host "$($disk.Index) - $($disk.Model)" -ForegroundColor Cyan
+        Write-Host "    Taille : $([math]::Round($disk.Size/1GB,2)) GB"
+
+        # Récupère les partitions liées à ce disque physique
+        $partitions = Get-CimAssociatedInstance -InputObject $disk -ResultClassName Win32_DiskPartition
+
+        foreach ($partition in $partitions) {
+            # Récupère les lecteurs logiques (lettres) liés à cette partition
+            $logicalDisks = Get-CimAssociatedInstance -InputObject $partition -ResultClassName Win32_LogicalDisk
+
+            foreach ($logicalDisk in $logicalDisks) {
+                $free = $logicalDisk.Size - $logicalDisk.FreeSpace
+                Write-Host "    -> $($logicalDisk.DeviceID) : $([math]::Round($free/1GB,2)) GB utilises / $([math]::Round($logicalDisk.Size/1GB,2)) GB" -ForegroundColor Green
+            }
+        }
+        $health = (Get-PhysicalDisk | Where-Object { $_.DeviceId -eq $disk.Index }).HealthStatus
+        Write-Host "    sante : $health"
+        Write-Host "    Status : $($disk.Status)"
+        Write-Host ""
+    }
+}
+
+
+$multiplelines = @"
+ ____  _  _  ____  ___  ____  ___ 
+/ ___)( \/ )/ ___)(  _ \(  __)/ __)
+\___ \ )  / \___ \ ) __/ ) _)( (__ 
+(____/(__/  (____/(__)  (____)\___)
+"@
+Write-Host "$multiplelines" -ForegroundColor Blue
+
+Write-Host ""
+Write-Host "Verification des permissions Administrateurs..."
+$admin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if ($admin -eq $True) {
+    Write-Host "Vous avez les permisions Administrateurs" -ForegroundColor Green
+
+}
+else {
+    Write-Host "Vous n'etes pas Administrateurs" -ForegroundColor red
+    Start-Sleep -Seconds 3
+    exit
+
+}
+
+prossesore_stat
+memory_stat
+Disk_stat
+
+Pause
+
+
+
